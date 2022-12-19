bc468e0f4217117173366f81cf5f67166477372e
Aydan Haqverdili <aydanhaqverdili@outlook.com>
2022-12-19 21:02:10 +0400

    mac os tutorial app ui done

---
+++ MacOS Projects/EggTimer/EggTimer/PrefsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by Aydan Haqverdili on 12/19/22.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    @IBOutlet weak var presetsPopup: NSPopUpButton!
+    @IBOutlet weak var customSlider: NSSlider!
+    @IBOutlet weak var customTextField: NSTextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+
+    @IBAction func popupValueChanged(_ sender: Any) {
+    }
+
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
+
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+    }
+}
